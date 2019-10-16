4465088b6b1bc1abdde984f1e6e37c20549556c7
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-15 22:51:32 -0400

    Added array with answers to the Javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+// Creating array storing all Word Guess answers.
+var kaiju = ["Godzilla", "Gigan", "Hedorah", "King Ghidorah", "Destroyah", "Mothra", "Rodan", "Mechagodzilla", "Kumonga", "King Kong"];
+
+document.onkeyup = function(event) {
+
+
+    
+
+};
+
