dc38d58e09dbaef28153ec78c4d407d601d75aaf
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-17 09:36:28 -0400

    Got word replacing underscores in wordDisplay box

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,65 +2,70 @@
 // Creating array storing all Word Guess answers.
 var kaiju = ["godzilla", "gigan", "hedorah", "ghidorah", "destroyah", "mothra", "rodan", "mechagodzilla", "kumonga", "kingkong"];
 //Choosing a random word from the Kaiju array    //Not seeing how this is working right with the loop right now
-var randomWord = Math.floor(Math.random()*10);
+var randomWord = Math.floor(Math.random() * kaiju.length);
 var wordGuess = kaiju[randomWord];
-var wordCount = wordGuess.length;
 var wins = 0;
 var chancesLeft = 10;
-
-
-
+// var display = [];
+var empty = [];
 
 console.log(wordGuess);
 
 
+
 // Created a for loop and an empty array to add _ _ _ to it for the empty letters / not sure which loop makes sense
-var empty = [];
+// Do I have ot put this in a function?????????????????
 
 for (var i = 0; i < wordGuess.length; i++) {
     empty.push("_");
+
+}
+// console.log(empty);
+
+
+
+var key = document.getElementById("wordDisplay")
+
+key.textContent = empty.join(" ");
+
+document.onkeyup = function (event) {
+
+  
+
+   console.log(event.key)
+
+   for (var j = 0; j < wordGuess.length; j++) {
+       if (wordGuess[j] === event.key) {
+           empty[j] = event.key;
+           key.textContent = empty.join(" ");
+
+       }
+   }
+
+
+
+   
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+
 }
 
 
 
-// var setup = function() {
-
-//     for (var i = 0; i < wordGuess.length; i++) {
-//         underScore[i] = "_ ";
-//         word = word + underScore[i];
-//     }
-//     document.getElementById("wrapper").innerHTML = word;
-//     word ="";
-
-// }
-
-
-
-
-
-document.getElementById(wrapper)
-
-
-
-
-
-document.onkeyup = function(event) {
-
-    var key = event.key.toLowerCase();
-
-
-
-    
-
-
-    
-
-
-};
-
-
-
-/* Psuedo Code 
+/* Psuedo Code
     -Computer chooses a random word from an array
     -Computer displays word with underscores in place of letters
     -User guesses a letter
