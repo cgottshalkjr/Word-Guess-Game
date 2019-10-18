3e65933fdc92aff0b203b816f406db5e173f2ea1
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-18 17:38:14 -0400

    Added audio and fixed other issues, tinkered with the css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,17 +12,17 @@ function resetGame() {
     chances.textContent = chancesLeft;
     wrong.textContent = wrongGuess.join(" ");
     keyPress.textContent = empty.join(" ");
-
 }
 
-// Was going to create a function here, but got hung up
-// function startUp () {
+//This function will play Godzilla Scream when you guess a word right
+function playSound() {
+    godzillaYell.play();
+}
 
-
-
-
-// }
-
+//This function will play loss audio.
+function youDead() {
+    youLost.play();
+}
 
 // Taking out Godzilla for now because I can't figure out how to make letterTally not count the L's together
 // Creating array storing all Word Guess answers.
@@ -41,18 +41,10 @@ var empty = [];
 var wrongGuess = [];
 var letterTally = 0;
 
-// console.log(wordGuess);
-
-
-
 // Created a for loop and an empty array to add _ _ _ to it for the empty letters 
-
 for (var i = 0; i < wordGuess.length; i++) {
     empty.push("_");
-
 }
-// console.log(empty);
-
 
 var startGame = document.getElementById("startGuess");
 var keyPress = document.getElementById("wordDisplay");
@@ -60,16 +52,16 @@ var wrong = document.getElementById("wrongLetters");
 var chances = document.getElementById("chances");
 var victory = document.getElementById("winDisplay");
 var youLose = document.getElementById("lossDisplay");
+var godzillaYell = document.getElementById("zillaAudio");
+var youLost = document.getElementById("youlose");
 
 keyPress.textContent = empty.join(" ");
-
-
 
 // Start of game, when key is clicked.
 document.onkeyup = function (event) {
 
     //Makes it so if you type anything that isn't in alphabet array it leaves function
-       var key = event.key
+    var key = event.key
     if (!alphabet.includes(key)) {
         return;
     }
@@ -77,28 +69,25 @@ document.onkeyup = function (event) {
     //Hiding start text when key is clicked
     startGame.textContent = "";
 
-    // console.log(event.key)
-
-    //I need to use this to somehow only let Users select letters
-    // if (alphabet.indexOf(event.key) > -1) {};
-        
     //If key is pressed and isn't in wordGuess it goes to wrong letters and chances left goes down by 1
     if (wordGuess.includes(event.key) == false) {
-        if(wrongGuess.includes(event.key) == false) {
+        if (wrongGuess.includes(event.key) == false) {
             wrongGuess.push(event.key);
             wrong.textContent = wrongGuess.join(" ");
             chancesLeft--;
             chances.textContent = chancesLeft;
 
-            if (chancesLeft === 0){
+            if (chancesLeft === 0) {
                 losses++;
+                youDead();
                 resetGame();
                 youLose.textContent = losses;
-                
-            }
 
+
+            }
         }
-    //if correct letter is guessed it replaces underscore   
+
+        //if correct letter is guessed it replaces underscore   
     } else {
         for (var j = 0; j < wordGuess.length; j++) {
             if (wordGuess[j] === event.key) {
@@ -109,39 +98,14 @@ document.onkeyup = function (event) {
             //if word is completed it adds one to Wins and resets game with new random word while retaining win
         } if (wordGuess.length === letterTally) {
             wins++;
+            playSound();
             resetGame();
         } victory.textContent = wins;
-    
-
-     // I need to finish game with make making game over and losses tally when chances get down to 0;
-     //I also need to make sure after key is in wrong guess to make sure it is not registered again.
-     // I will also add image and sounds
-
-
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
 
-
+//The only thing I didn't get to fix was that I couldn't put godzilla in because it would register the L's together as one letter and would mess up the letterTally variable and it would not register a win, it would reload but not register a win.
 
 /* Psuedo Code
     -Computer chooses a random word from an array
@@ -154,3 +118,25 @@ document.onkeyup = function (event) {
     -If user uses all 10 trys, game over
     -Start with another random word
 */
+
+
+//This wouldn't work for me.
+
+// function youLost() {
+//     var audioLoss = [crySound, soDumbSound, dumbSound, stupidSound];
+//     var lossPlay = Math.floor(Math.random() * audioLoss.length);
+//     lossPlay.play();
+// }
+
+// Was going to create a function here, but got hung up
+// function startUp () {
+// }
+
+// var stupidSound = document.getElementById("stupid");
+// var crySound = document.getElementById("cry");
+// var soDumbSound = document.getElementById("sodumb");
+
+// console.log(event.key)
+// console.log(empty);
+// console.log(wordGuess);
+
