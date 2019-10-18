c1022d8a35cfbf40103df13592e34fd7a12400a9
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-17 23:41:20 -0400

    Made it so keys that aren't a letter do no register and tweeked some other items

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,32 +12,36 @@ function resetGame() {
     chances.textContent = chancesLeft;
     wrong.textContent = wrongGuess.join(" ");
     keyPress.textContent = empty.join(" ");
-    
-}
-
-//This will stop the game when the the number of guesses runs out
-function loseGame () {
 
 }
+
+// Was going to create a function here, but got hung up
+// function startUp () {
+
+
+
+
+// }
 
 
 // Taking out Godzilla for now because I can't figure out how to make letterTally not count the L's together
 // Creating array storing all Word Guess answers.
 var kaiju = ["gorosaurus", "gigan", "hedorah", "ghidorah", "destroyah", "mothra", "rodan", "anguirius", "kumonga", "kingkong"];
 
-alphaBet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //Choosing a random word from the Kaiju array    
 var randomWord = Math.floor(Math.random() * kaiju.length);
 var wordGuess = kaiju[randomWord];
 var wins = 0;
+var loses = 0;
 var chancesLeft = 10;
 var empty = [];
 var wrongGuess = [];
 var letterTally = 0;
 
-console.log(wordGuess);
+// console.log(wordGuess);
 
 
 
@@ -54,25 +58,37 @@ var startGame = document.getElementById("startGuess");
 var keyPress = document.getElementById("wordDisplay");
 var wrong = document.getElementById("wrongLetters");
 var chances = document.getElementById("chances");
-var victory = document.getElementById("winWin");
+var victory = document.getElementById("winDisplay");
 var youLose = document.getElementById("loss");
 
 keyPress.textContent = empty.join(" ");
 
 
 
-
+// Start of game, when key is clicked.
 document.onkeyup = function (event) {
 
+    //Makes it so if you type anything that isn't in alphabet array it leaves function
+       var key = event.key
+    if (!alphabet.includes(key)) {
+        return;
+    }
+
+    //Hiding start text when key is clicked
     startGame.textContent = "";
 
     // console.log(event.key)
 
+    //I need to use this to somehow only let Users select letters
+    // if (alphabet.indexOf(event.key) > -1) {};
+        
+    //If key is pressed and isn't in wordGuess it goes to wrong letters and chances left goes down by 1
     if (wordGuess.includes(event.key) == false) {
         wrongGuess.push(event.key);
         wrong.textContent = wrongGuess.join(" ");
         chancesLeft--;
         chances.textContent = chancesLeft;
+    //if correct letter is guessed it replaces underscore   
     } else {
         for (var j = 0; j < wordGuess.length; j++) {
             if (wordGuess[j] === event.key) {
@@ -80,11 +96,18 @@ document.onkeyup = function (event) {
                 keyPress.textContent = empty.join(" ");
                 letterTally++;
             }
+            //if word is completed it adds one to Wins and resets game with new random word while retaining win
         } if (wordGuess.length === letterTally) {
             wins++;
             resetGame();
         } victory.textContent = wins;
-        
+    
+
+     // I need to finish game with make making game over and losses tally when chances get down to 0;
+     //I also need to make sure after key is in wrong guess to make sure it is not registered again.
+     // I will also add image and sounds
+
+
     }
 
 
